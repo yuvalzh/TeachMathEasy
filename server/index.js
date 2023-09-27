42ded2fe1a1474cf63b893c5e6c988a6f9699478
yuvalzh <yuvi.zehavi@gmail.com>
2023-09-28 02:32:20 +0300

    add lesson create flow - post request

--- server/index.js
+++ server/index.js
@@ -1,27 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
+import routes from "./routes/index.js";
+import databaseConnect from "./database/index.js";
 
-import lessonRouter from "./routes/lesson.js";
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("/lesson", lessonRouter);
+app.use(cors());
+
+app.use("/api", routes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://TeachMathEasy:Aa123456@cluster0.d9jve9k.mongodb.net/";
-const PORT = process.env.PORT || 5000;
+// connect to database
+databaseConnect();
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`))
-  )
-  .catch((error) => console.log(`${error} did not connect`));
-
-//mongoose.set("useFindAndModify", false); - check what this ! 15:03
+app.listen(port, () => console.log(`Server Running on Port:${port}`));
